43e6301e3512158c10d6bf4d2de612549996fa6a
posypov <osypovpavel@gmail.com>
2023-07-10 22:30:09 +0300

    Fixed eslint issues

--- src/modules/home-page/chat-window/types.ts
+++ src/modules/home-page/chat-window/types.ts
@@ -3,11 +3,11 @@ import { TConversation } from '@/server/types';
 
 export type TUseChatWindowLogic = {
   value: string;
-  onInput: (_: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onInput: (_event: React.ChangeEvent<HTMLTextAreaElement>) => void;
   inputRef: React.MutableRefObject<HTMLTextAreaElement | null>;
   chatHistoryRef: React.RefObject<HTMLDivElement>;
   conversation: TConversation;
-  onEnterPress: (_: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+  onEnterPress: (_event: React.KeyboardEvent<HTMLTextAreaElement>) => void;
   onClickSendBtn: () => void;
   onClickCleanDB: () => void;
   showConversationLoading: boolean;
