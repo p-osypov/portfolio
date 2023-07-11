690ee3364108f45b9fafa47acdddcfb94a5a8471
Pavlo Osypov <osypovpavel@gmail.com>
2023-07-11 13:29:07 +0300

    Develop (#4)

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 import { sendMessageToChatGPT } from './chatGPT';
 const router = Router();
 
-router.post('/api/gpt', async (req: Request, res: Response) => {
+router.post('/api/conversation', async (req: Request, res: Response) => {
   try {
     const response = await sendMessageToChatGPT(req.body.conversation);
     res.status(200).send(response);
