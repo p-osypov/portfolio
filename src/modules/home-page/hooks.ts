e74ee323ea26766a13222c6020fd066d20307eb2
posypov <osypovpavel@gmail.com>
2023-07-12 19:48:06 +0300

    Added bg space

--- src/modules/home-page/hooks.ts
+++ src/modules/home-page/hooks.ts
@@ -4,13 +4,31 @@ import { TUseHomePageLogic } from '@/modules/home-page/types';
 export const useHomePageLogic = (): TUseHomePageLogic => {
   // ToDo: use server to get systemIsReady(client started work with system)
   const [systemIsReady, setSystemIsReady] = useState(false);
-
   const onClickPowerButton = async () => {
     setTimeout(() => {
       // Finish all animations before show chat screen
       setSystemIsReady(true);
     }, 1000);
   };
-  useEffect(() => {}, [systemIsReady]);
+  useEffect(() => {
+    let size = 300;
+    let isDecreasing = true;
+    setInterval(() => {
+      if (isDecreasing) {
+        if (size > 200) {
+          size -= 0.2;
+        } else {
+          isDecreasing = false;
+        }
+      } else {
+        if (size < 300) {
+          size += 0.2;
+        } else {
+          isDecreasing = true;
+        }
+      }
+      document.body.style.backgroundSize = `${size}px`;
+    }, 100);
+  }, []);
   return { systemIsReady, onClickPowerButton };
 };
