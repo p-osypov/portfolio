348ffbf00d77a4dc0402089129f8aa0ba516d9d1
posypov <osypovpavel@gmail.com>
2023-07-12 20:46:20 +0300

    Added locale-storage hook

--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 const get = <T>(key: string, initialData: T): T => {
   const data = localStorage.getItem(key);
   return data ? JSON.parse(data) : initialData;
@@ -19,4 +20,17 @@ export enum LOC_STORAGE_KEYS {
   // eslint-disable-next-line no-unused-vars
   conversation = 'conversation',
 }
+export const useLocaleStorage = <T>(key: string): T | null => {
+  const [state, setState] = useState<T | null>(null);
+  useEffect(() => {
+    window.addEventListener('storage', (event) => {
+      if (key === event.key) {
+        setState(() => JSON.parse(event.newValue as string) as T);
+      }
+    });
+    locStorage.get(key, null);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return state;
+};
 export default locStorage;
